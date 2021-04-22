b504a20f845d5d8c40f8c5f9e08a06f18f1ee962
Dasha-290110 <77583048+Dasha-290110@users.noreply.github.com>
2021-04-22 22:25:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var nextslide = [  "https://st.depositphotos.com/1724125/2693/v/950/depositphotos_26930203-stock-illustration-cartoon-dad.jpg",
+"https://c8.alamy.com/comp/M737J0/cartoon-beauty-mother-standing-character-M737J0.jpg",
+"https://www.clipartmax.com/png/small/231-2316381_cute-cartoon-girl-with-pigtails-black-hair-and-dark-worlds-best-little.png",
+"https://i.pinimg.com/originals/d8/ac/a9/d8aca934c60c7f9392f8828e50b03f4b.jpg",
+"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/57c75f4e-3f45-4214-bb1d-3a6d4c983358/d4gga57-92f641a1-d9d6-4482-beae-5416618bc0e1.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNTdjNzVmNGUtM2Y0NS00MjE0LWJiMWQtM2E2ZDRjOTgzMzU4XC9kNGdnYTU3LTkyZjY0MWExLWQ5ZDYtNDQ4Mi1iZWFlLTU0MTY2MThiYzBlMS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.rG69SPkEsXwpwtQwxfpVStFlTLWaGuJzpjWUShOMTcQ"
+
+  ]
+   
+
+    
